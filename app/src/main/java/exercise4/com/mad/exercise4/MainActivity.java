7e0f91178b27ea8117df692cdecd04887e136bc2
AndrewRafe <andrew.rafe@outlook.com>
2016-08-26 15:16:30 +1000

    Added spinner to be able to filter the lengths of the jokes Updated the download joke from internet method to take a joke length filter Fixed some resource strings and constants to conform to android standards

--- app/src/main/java/exercise4/com/mad/exercise4/MainActivity.java
+++ app/src/main/java/exercise4/com/mad/exercise4/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -24,16 +28,23 @@ import java.net.URLConnection;
 public class MainActivity extends AppCompatActivity {
 
     public static final String INPUT_ERROR_TAG = "IO ERROR";
+    public static final String URL_STRING = "http://www-staff.it.uts.edu.au/~rheise/sarcastic.cgi";
+    public static final String URL_SHORT_FILTER_OPTION = "?len=Short";
+    public static final String URL_MEDIUM_FILTER_OPTION = "?len=Medium";
+    public static final String URL_LONG_FILTER_OPTION = "?len=Long";
+    public static final int NUM_JOKES = 3;
 
     private TextView jokeText;
     private Button oneJokeButton;
     private Button threeJokesButton;
+    private Spinner jokeLengthSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpJokeLengthSpinner();
         jokeText = (TextView) findViewById(R.id.main_joke_tv);
         oneJokeButton = (Button) findViewById(R.id.main_one_joke_btn);
         threeJokesButton = (Button) findViewById(R.id.main_three_jokes_btn);
@@ -46,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                new Download1JokeAsyncTask().execute();
+                new Download1JokeAsyncTask(jokeLengthSpinner.getSelectedItem().toString()).execute();
             }
         });
 
@@ -58,18 +69,66 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                new DownloadNJokesAsyncTask(3).execute();
+                new DownloadNJokesAsyncTask(NUM_JOKES, jokeLengthSpinner.getSelectedItem().toString()).execute();
             }
         });
-
 
     }
 
     /**
-     * A method to retrieve one joke from the internet
+     * Helper method to populate and initiate the joke length spinner
      */
-    public String readOneJoke() throws IOException {
-        URL url = new URL("http://www-staff.it.uts.edu.au/~rheise/sarcastic.cgi");
+    public void setUpJokeLengthSpinner() {
+        jokeLengthSpinner = (Spinner) findViewById(R.id.main_joke_length_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.joke_length, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        jokeLengthSpinner.setAdapter(adapter);
+        jokeLengthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            /**
+             * Will display a toast notification to alert the user to the length of joke
+             * selected from the spinner.
+             * @param parent
+             * @param view
+             * @param position
+             * @param id
+             */
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, (jokeLengthSpinner.getSelectedItem().toString()
+                        + " " + getString(R.string.joke_length_toast_alert)), Toast.LENGTH_SHORT).show();
+            }
+
+            /**
+             * When nothing is selected in the spinner no toast notification needs to be generated
+             * as no options have been change
+             * @param parent
+             */
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //INTENTIONALLY BLANK
+            }
+
+        });
+    }
+
+    /**
+     * A method to retrieve one joke from the internet given a specific joke length
+     */
+    public String readOneJoke(String jokeLength) throws IOException {
+        String filterOption;
+        if (jokeLength.equals(getString(R.string.short_joke_length_option))) {
+            filterOption = URL_SHORT_FILTER_OPTION;
+        } else if (jokeLength.equals(getString(R.string.medium_joke_length_option))) {
+            filterOption = URL_MEDIUM_FILTER_OPTION;
+        } else {
+            filterOption = URL_LONG_FILTER_OPTION;
+        }
+
+        URL url = new URL(URL_STRING + filterOption);
         URLConnection conn = url.openConnection();
         //Obtain the Input Stream
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -86,6 +145,11 @@ public class MainActivity extends AppCompatActivity {
     private class Download1JokeAsyncTask extends AsyncTask<Void, Void, String> {
 
         private ProgressDialog loadOneJokeProgress;
+        private String lengthFilterOption;
+
+        protected Download1JokeAsyncTask(String lengthFilterOption) {
+            this.lengthFilterOption = lengthFilterOption;
+        }
 
         /**
          * Initiates and shows the progress dialog for loading one joke
@@ -104,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(Void... params) {
             String result;
             try {
-                result = readOneJoke();
+                result = readOneJoke(lengthFilterOption);
                 return result;
             } catch(IOException inException) {
                 Log.d(INPUT_ERROR_TAG, "IOException thrown while trying" +
@@ -128,16 +192,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * A class that downloads and displays a given number of jokes
+     * A class that downloads and displays a given number of jokes and a joke length
      * The class extends AsyncTask and runs on a background thread
      */
     private class DownloadNJokesAsyncTask extends AsyncTask<Void, Integer, String[]> {
 
         private ProgressDialog loadNJokesProgress;
         private int numJokes;
+        private String lengthFilterOption;
 
-        protected DownloadNJokesAsyncTask(int numJokes) {
+        /**
+         * Constructor class that stores the number of jokes to be retrieved and the filter
+         * option for the joke length
+         * @param numJokes
+         * @param lengthFilterOption
+         */
+        protected DownloadNJokesAsyncTask(int numJokes, String lengthFilterOption) {
             this.numJokes = numJokes;
+            this.lengthFilterOption = lengthFilterOption;
         }
 
         /**
@@ -164,7 +236,7 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < numJokes; i++) {
                 publishProgress(i);
                 try {
-                    jokes[i] = readOneJoke();
+                    jokes[i] = readOneJoke(lengthFilterOption);
                 } catch (IOException readException) {
                     Log.d(INPUT_ERROR_TAG, "IOException thrown while trying" +
                             " to read one joke");
